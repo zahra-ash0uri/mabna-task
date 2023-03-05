bad6049d565f78623fdfad5dc95bcc7d093ac9b7
Zahra Ashouri <zahraashouri97@gmail.com>
2023-03-05 15:47:27 +0330

    add api urls in main

--- controllers/trade.go
+++ controllers/trade.go
@@ -1,17 +1,31 @@
 package controllers
 
 import (
+	"context"
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zahra-ash0uri/mabna-task/models"
 )
 
-func OrderShowByID(c *gin.Context) {
+func TradeShowByRecent(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
 
-	query := "SELECT i.Name, t.DateEn FROM trade t join on instrument i where t.InstrumentID = i.Id GROUPBY I.NAME ORDER BY t.DateEn;"
-	fmt.Println(query)
-	// if err := models.Connection.Exec(context.Background(), "SELECT * from orders where order_id = $1", order_id).Scan(&orders).Error; err != nil {
+	rows, err := models.Connection.Query(context.Background(), "SELECT i.Name, t.DateEn FROM trade t join on instrument i where t.InstrumentID = i.Id GROUPBY I.NAME ORDER BY t.DateEn;")
+	if err != nil {
+		log.Fatal("error while executing query")
+	}
+
+	for rows.Next() {
+		values, err := rows.Values()
+		fmt.Println(values...)
+		if err != nil {
+			log.Fatal("error while iterating dataset")
+		}
+	}
+
+	// if err := models.Connection.Query(context.Background(), "SELECT * from orders where order_id = $1", order_id).Scan(&orders).Error; err != nil {
 	// 	fmt.Printf("error show order by id: %3v \n", err)
 	// 	c.AbortWithStatus(http.StatusInternalServerError)
 	// 	return
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/zahra-ash0uri/mabna-task/controllers"
 	"github.com/zahra-ash0uri/mabna-task/models"
 )
 
 func main() {
+	fmt.Printf("Started at : %3v \n", time.Now())
+
 	conn, err := models.ConnectToPostgres()
 	if err != nil {
 		fmt.Println("Can not connect to database...")
 		return
 	}
 	fmt.Println(conn)
-	defer conn.Close(context.Background())
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	api := router.Group("/api")
+	api.GET("/show", controllers.TradeShowByRecent)
+	router.Run(":4000")
 
 }
